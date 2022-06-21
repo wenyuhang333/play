f2309cf8dcc46bf87c4396577b7eac0ac4fb2d46
wenyuhang333 <wenyuhang333@sina.com>
2022-06-21 17:47:35 +0800

    create Test

---
+++ src/main/java/com/atguigu/git/Test.java
@@ -0,0 +1,18 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName : Test
+ * @Author : Devil
+ * @Date : 2022/6/21 17:44
+ * @Description :
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
+    }
+}
